957c821506da1bcbe90f4b7dc54468a1485af624
meassamrong <meassamrong99@gmail.com>
2023-06-13 06:14:10 +0700

    last update 13-06-2023

---
+++ g63/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version "adamant"
+game "gta5"
+
+files {
+	"caraddoncontentunlocks.meta",
+	-- "carcols.meta",
+	"carvariations.meta",
+	"dlctext.meta",
+	"handling.meta",
+	"vehicles.meta",
+	"audioconfig/*.dat151.rel",
+	"audioconfig/*.dat54.rel",
+	"audioconfig/*.dat10.rel",
+	"sfx/**/*.awc"
+}
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+-- data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
+data_file "AUDIO_SYNTHDATA" "audioconfig/lg101timcamz28_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/lg101timcamz28_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg101timcamz28_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/schafter3"
