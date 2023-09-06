7f368b9e3b43b7c5bcc0bf5de2ac2cebc8580840
meassamrong <meassamrong99@gmail.com>
2023-09-07 00:31:02 +0700

    update

---
+++ Mazda-RX-7-FC3S/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+-- Add your resource information here
+name 'Mazda rx7-FC3s'
+description 'this is vehicle customized'
+version '1.0'
+
+-- Specify which files to include
+files {
+    'carcols.meta',
+    'carvariations.meta',
+    'dlctext.meta',
+    'handling.meta',
+    'mfccontentunlocks.meta',
+    'vehiclelayouts.meta',
+    'vehiclelayoutsf.meta',
+    'vehicles.meta',
+}
+
+-- List the data files to be included
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'MFC_CONTENT_UNLOCK_FILE' 'mfccontentunlocks.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE_F' 'vehiclelayoutsf.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+
